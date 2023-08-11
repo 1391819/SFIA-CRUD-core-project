b164947c45f62f07a5961e044fe69ef8d6e58990
1391819 <roberto.nacu@gmail.com>
2023-08-11 16:02:56 +0100

    RN: added home/index endpoint

--- application/routes/routes.py
+++ application/routes/routes.py
@@ -1,6 +1,29 @@
 from application import app, db
 from application.models import Items, Categories
 from flask import render_template
+import random
+
+
+@app.route("/")
+@app.route("/home")
+def index():
+    # a bit of trickery, we get random items
+    total_items = Items.query.count()
+
+    # number of items we want to retrieve
+    # for now, it's pretty much the entire database
+    n_items = 5
+
+    # generating random indices for selecting random items
+    random_indices = random.sample(range(1, total_items + 1), n_items)
+
+    # we query the database for the items with the generated indices
+    random_items = Items.query.filter(Items.item_id.in_(random_indices)).all()
+
+    print(random_items)
+
+    # rendering appropriate template
+    return render_template("index.html", items=random_items)
 
 
 @app.route("/items")
