43cea2208f7bee4e7931b9cfb946bc1ab06ad978
1391819 <roberto.nacu@gmail.com>
2023-08-11 09:32:45 +0100

    RN: added models layout based on classes definition

---
+++ application/models.py
@@ -0,0 +1,21 @@
+from application import db
+
+
+class Items:
+    pass
+
+
+class Orders:
+    pass
+
+
+class Categories:
+    pass
+
+
+class OrdersItems:
+    pass
+
+
+class Customers:
+    pass
